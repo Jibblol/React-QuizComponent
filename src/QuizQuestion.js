f9f241b14152b3d3bbbd860621026342c09bdb2d
unknown <jibblol@gmail.com>
2020-04-21 10:51:28 +0200

    module 6 start

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import QuizQuestionButton from './QuizQuestionButton.js'
+import QuizQuestionButton from "./QuizQuestionButton.js";
 
 class QuizQuestion extends Component {
   constructor(props) {
@@ -14,7 +14,10 @@ class QuizQuestion extends Component {
         </section>
         <section className="buttons">
           <ul>
-              <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
+            {this.props.quiz_question.answer_options.map(answer_option, index)}
+            <QuizQuestionButton
+              button_text={this.props.quiz_question.answer_options[0]}
+            />
           </ul>
         </section>
       </main>
