812130fe557472fdf932cda680b6b6f5c40ec9f0
unknown <jibblol@gmail.com>
2020-04-21 10:37:30 +0200

    piss

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import QuizQuestion from './QuizQuestion.jsx'
-import QuizEnd from './QuizEnd.jsx'
+import QuizQuestion from './QuizQuestion.js'
+import QuizEnd from './QuizEnd.js'
 
 let quizData = require('./quiz_data.json')
 
